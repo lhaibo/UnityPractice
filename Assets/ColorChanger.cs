4c0c3d6b98f8e81b2c3cc4fcc147be233b4f6205
lihaibo <1170946850@qq.com>
2021-07-28 19:37:48 +0800

    Color Changer

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Renderer>().material.color = GetRandomColor();
+        }
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(   UnityEngine.Random.Range(0, 1f), 
+                            UnityEngine.Random.Range(0, 1f), 
+                            UnityEngine.Random.Range(0, 1f));
+    }
+}
